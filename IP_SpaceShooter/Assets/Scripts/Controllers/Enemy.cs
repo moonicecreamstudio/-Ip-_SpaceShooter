04e33302741b290b6ce304e2f83e040e5eeef077
moonicecreamstudio <moonicecreamstudio@gmail.com>
2024-09-23 16:55:58 -0400

    Task 2A and 2B Completed
    Added enemy movement where it would follow the player and stop within a certain distance.

--- IP_SpaceShooter/Assets/Scripts/Controllers/Enemy.cs
+++ IP_SpaceShooter/Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using static UnityEngine.GraphicsBuffer;
+using Codice.CM.Common;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject player;
+    float movementSpeed = 1f;
+    float maxEnemyDistance = 5f;
     private void Update()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        EnemyMovement();
+        
     }
 
+    public void EnemyMovement()
+    {
+        var step = movementSpeed * Time.deltaTime;
+        float enemyToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (enemyToPlayer > maxEnemyDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
 }
